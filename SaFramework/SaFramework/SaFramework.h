a5523e046de5428b597362480fd51dc418449507
saurabh <saurabh.iosdevlpr@gmail.com>
2019-03-11 18:18:04 +0530

    finished.

---
+++ SaFramework/SaFramework/SaFramework.h
@@ -0,0 +1,19 @@
+//
+//  SaFramework.h
+//  SaFramework
+//
+//  Created by iMAC on 11/03/19.
+//  Copyright © 2019 Saurabh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SaFramework.
+FOUNDATION_EXPORT double SaFrameworkVersionNumber;
+
+//! Project version string for SaFramework.
+FOUNDATION_EXPORT const unsigned char SaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SaFramework/PublicHeader.h>
+
+
---
+++ SaFramework/SaFramework/Services.swift
@@ -0,0 +1,22 @@
+//
+//  Services.swift
+//  SaFramework
+//
+//  Created by iMAC on 11/03/19.
+//  Copyright © 2019 Saurabh. All rights reserved.
+//
+
+import Foundation
+
+
+public class Services {
+    
+    
+    public func doSomthing() -> String {
+        return "Hey! this is sample framework to integrate for test. \nMade by Saurabh Chaudhari. \niOS App Developer."
+    }
+    
+    public class func doEverything() -> String {
+        return "this is class function."
+    }
+}
